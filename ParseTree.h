a5002a50c85a892f1983c4cbc0c7cdf9afde09bb
chupotle <tcchu97@gmail.com>
2016-10-19 19:55:22 -0400

    trying to get it to work

--- ParseTree.h
+++ ParseTree.h
@@ -3,10 +3,10 @@
 #include <string.h>
 #define FAILED NULL
 typedef struct NODE *Tree;
-struct NODE {
+typedef struct {
   char label;
   Tree leftmostChild, rightSibling;
-};
+}NODE;
 Tree parseTree; /* holds the result of the parse */
 char *input; /* current position in input string */
 char prev;
@@ -14,9 +14,11 @@ char curr;
 char ahead;
 int parens;
 Tree tempL, tempR;
-Tree makeNode0(char x);
-Tree makeNode1(char x, Tree t);
-Tree makeNode2(char x, Tree t1, Tree t2);
-Tree makeNode3(char x, Tree t1, Tree t2, Tree t3);
-char lookahead();
-Tree Pls();
+extern Tree makeNode0(char x);
+extern Tree makeNode1(char x, Tree t);
+extern Tree makeNode2(char x, Tree t1, Tree t2);
+extern Tree makeNode3(char x, Tree t1, Tree t2, Tree t3);
+extern int lookahead(char c);
+extern int matchTerminal(char c);
+extern char peekNext();
+extern Tree* Pls();
